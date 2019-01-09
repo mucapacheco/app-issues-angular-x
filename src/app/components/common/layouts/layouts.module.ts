14965997f9a2aa3ce89211a39731109b8dacf922
Samuel Pacheco Pereira <spereira@unisuam.edu.br>
2019-01-09 11:07:24 -0200

    Corrigindo Path linux

--- src/app/components/common/layouts/layouts.module.ts
+++ src/app/components/common/layouts/layouts.module.ts
@@ -6,12 +6,12 @@ import {BsDropdownModule} from 'ngx-bootstrap';
 
 import {BasicLayoutComponent} from "./basicLayout.component";
 import {BlankLayoutComponent} from "./blankLayout.component";
-import {TopNavigationLayoutComponent} from "./topNavigationlayout.component";
 
 import {NavigationComponent} from "./../navigation/navigation.component";
 import {FooterComponent} from "./../footer/footer.component";
 import {TopNavbarComponent} from "./../topnavbar/topnavbar.component";
 import {TopNavigationNavbarComponent} from "./../topnavbar/topnavigationnavbar.component";
+import {TopNavigationLayoutComponent} from './topNavigationLayout.component';
 
 
 @NgModule({
