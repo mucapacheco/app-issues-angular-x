65044512c6e7487ff60ed122359aa9c561a10159
samuelpacheco <spereira@unisuam.edu.br>
2019-01-09 15:25:13 -0200

    Atualizando Projeto Issue Tracker

--- src/app/views/issue-tracker/issue-list-box.component.ts
+++ src/app/views/issue-tracker/issue-list-box.component.ts
@@ -15,21 +15,72 @@ declare var jQuery: any;
 
 export class IssueListBoxComponent {
 
-  public data = [
-    {'concluido': true, 'estimativa': 4, 'tipo': 'dev', 'realizado': null, 'atividade': 'Protótipo da Tela de Usuários'},
-    {'concluido': true, 'estimativa': 4, 'tipo': 'dev', 'realizado': null, 'atividade': 'Protótipo da Tela de Sprints'},
-    {'concluido': true, 'estimativa': 4, 'tipo': 'dev', 'realizado': null, 'atividade': 'Protótipo da Tela de Projetos'},
-    {'concluido': true, 'estimativa': 16, 'tipo': 'dev', 'realizado': null, 'atividade': 'Protótipo da Tela de Tarefas'},
-    {'concluido': true, 'estimativa': 8, 'tipo': 'dev', 'realizado': null, 'atividade': 'Tela de Usuários'},
-    {'concluido': true, 'estimativa': 8, 'tipo': 'dev', 'realizado': null, 'atividade': 'Tela de Sprints'},
-    {'concluido': true, 'estimativa': 8, 'tipo': 'dev', 'realizado': null, 'atividade': 'Tela de Projetos'},
-    {'concluido': false, 'estimativa': 12, 'tipo': 'dev', 'realizado': null, 'atividade': 'Tela de Tarefas'},
-    {'concluido': false, 'estimativa': 4, 'tipo': 'dev', 'realizado': null, 'atividade': 'Deploy'},
-    {'concluido': false, 'estimativa': 12, 'tipo': 'dev', 'realizado': null, 'atividade': 'Teste'}
+  public tipos = ['DEV', 'TESTE', 'DEPLOY'];
+
+  public data: any = [
+    // {'concluido': true, 'estimativa': 4, 'tipo': 'DEV', 'realizado': null, 'atividade': 'Protótipo da Tela de Usuários'},
+    // {'concluido': true, 'estimativa': 4, 'tipo': 'DEV', 'realizado': null, 'atividade': 'Protótipo da Tela de Sprints'},
+    // {'concluido': true, 'estimativa': 4, 'tipo': 'DEV', 'realizado': null, 'atividade': 'Protótipo da Tela de Projetos'},
+    // {'concluido': true, 'estimativa': 16, 'tipo': 'DEV', 'realizado': null, 'atividade': 'Protótipo da Tela de Tarefas'},
+    // {'concluido': true, 'estimativa': 8, 'tipo': 'DEV', 'realizado': null, 'atividade': 'Tela de Usuários'},
+    // {'concluido': true, 'estimativa': 8, 'tipo': 'DEV', 'realizado': null, 'atividade': 'Tela de Sprints'},
+    // {'concluido': true, 'estimativa': 8, 'tipo': 'DEV', 'realizado': null, 'atividade': 'Tela de Projetos'},
+    // {'concluido': false, 'estimativa': 12, 'tipo': 'DEV', 'realizado': null, 'atividade': 'Tela de Tarefas'},
+    // {'concluido': false, 'estimativa': 4, 'tipo': 'DEV', 'realizado': null, 'atividade': 'Deploy'},
+    // {'concluido': false, 'estimativa': 12, 'tipo': 'DEV', 'realizado': null, 'atividade': 'Teste'}
   ];
 
 
   changed(atividade, item) {
     console.log(atividade, item);
   }
+
+  nextTipo(tipo) {
+    let index = this.tipos.indexOf(tipo) + 1;
+    if (index >= this.tipos.length) {
+      index = 0;
+    }
+
+    return this.tipos[index];
+
+  }
+
+  addAtividade() {
+
+    const node = this.focusAtividadeVazia();
+
+    if (node) {
+      return null;
+    }
+
+    this.data.push({'tipo': this.tipos[0]});
+
+    setTimeout(() => {
+      // @ts-ignore
+      document.querySelector('[key=atividade_' + (this.data.length - 1) + ']').focus();
+    }, 100);
+  }
+
+  removeItem(index) {
+    this.data.splice(index, 1);
+  }
+
+  focusAtividadeVazia() {
+    let node = null;
+    // @ts-ignore
+    document.querySelectorAll('[tipo=atividade]').forEach(a => {
+      if (!a.innerHTML.trim()) {
+        node = a;
+      }
+    });
+
+    if (!node) {
+      return;
+    }
+
+    node.focus();
+
+    return node;
+  }
+
 }
